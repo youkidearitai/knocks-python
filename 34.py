0e3486ad4fd00e2b3b61ddcad6027387f2eb5c20
youkidearitai <youkidearitai+github@gmail.com>
2017-05-25 01:48:58 +0900

    34.pyの「の」の検索方法をOut Of Rangeから回避する

--- 34.py
+++ 34.py
@@ -5,7 +5,7 @@ import mecab_04.mecab_init
 
 mapping = mecab_04.mecab_init.mapping()
 
-for index in range(0, len(mapping)):
+for index in range(2, len(mapping) - 2):
     if mapping[index]['surface'] == 'の' and \
             mapping[index - 1]['pos'] == '名詞' and \
             mapping[index + 1]['pos'] == '名詞':
