8d3257dbf01df7eb56bde67d0a24fcda25c525dc
youkidearitai <youkidearitai@gmail.com>
2017-05-25 01:20:13 +0900

    基本形(base)のみ抽出

---
+++ 32.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+import mecab_04.mecab_init
+
+mapping = mecab_04.mecab_init.mapping()
+
+surfaces = [word for word in mapping if word['pos'] == '動詞']
+for surface in surfaces:
+    print(surface['base'])
+
