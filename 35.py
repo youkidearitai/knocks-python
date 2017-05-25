d892640301844cf64238fb4a4913cdb0d9227741
youkidearitai <youkidearitai+github@gmail.com>
2017-05-25 23:21:45 +0900

    35.py 多分名詞の最長一致

---
+++ 35.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+import mecab_04.mecab_init
+from itertools import groupby
+
+mapping = mecab_04.mecab_init.mapping()
+
+nouns = []
+listnouns = []
+for index in range(0, len(mapping)):
+    if mapping[index]['pos'] == '名詞' and mapping[index + 1]['pos'] == '名詞':
+        nouns.append(mapping[index]['surface'])
+    else:
+        if len(nouns) > 1:
+            listnouns.append(nouns)
+        nouns = []
+
+for noun in listnouns:
+    print(" ".join(noun))
