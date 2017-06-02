a01040c0974fde32f1e854be76498966072b932c
youkidearitai <youkidearitai+github@gmail.com>
2017-06-03 02:40:34 +0900

    31.pyの出力方法を変更

--- 31.py
+++ 31.py
@@ -5,6 +5,6 @@ import mecab_04.mecab_init
 
 mapping = mecab_04.mecab_init.mapping()
 
-surfaces = [word['surface'] for word in mapping if word['pos'] == '動詞']
+surfaces = [word for word in mapping if word['pos'] == '動詞']
 for surface in surfaces:
-    print(surface)
+    print(surface['surface'])
