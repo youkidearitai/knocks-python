52483efda5dace2170fae73009e5adddf2c8498d
youkidearitai <youkidearitai+github@gmail.com>
2017-06-03 23:49:31 +0900

    40.py

---
+++ 40.py
@@ -0,0 +1,15 @@
+#!/usr/env/bin python
+# -*- coding: utf-8 -*-
+
+import cabocha_05.cabocha_init
+
+if __name__ == '__main__':
+    for morph in cabocha_05.cabocha_init.cabocha_file_open()[2]:
+        print(
+            "surface: {0} base: {1} pos: {2} pos1: {3}".format(
+                morph.surface,
+                morph.base,
+                morph.pos,
+                morph.pos1
+            )
+        )
--- cabocha_05/cabocha_init.py
+++ cabocha_05/cabocha_init.py
@@ -2,6 +2,7 @@
 # -*-coding: utf-8 -*-
 
 import CaboCha
+import re
 
 def init():
     parser = CaboCha.Parser()
@@ -9,6 +10,46 @@ def init():
         tree = parser.parse(fp.read())
         with open("neko.txt.cabocha", "w") as cfp:
             cfp.write(tree.toString(CaboCha.FORMAT_LATTICE))
+
+class Morph(object):
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+def cabocha_file_open():
+    description = re.compile("\* [0-9]+ \-?[0-9]+D [0-9]+\/[0-9]+ \-?[0-9]+(\.[0-9]+)?")
+    morph_lists = []
+    tmp = []
+
+    with open("neko.txt.cabocha") as fp:
+        fp.readline()
+
+        for line in fp:
+            line = line.rstrip()
+            if line == "EOS":
+                break
+
+            if description.search(line) is not None:
+                morph_lists.append(tmp)
+                tmp = []
+            else:
+                lists = re.split("[\t,]", line)
+                try:
+                    tmp.append(
+                        Morph(
+                            lists[0],
+                            lists[7],
+                            lists[1],
+                            lists[2]
+                        )
+                    )
+                except IndexError as indexerror:
+                    print(line)
+                    raise indexerror
+
+    return morph_lists
 
 if __name__ == '__main__':
     init()
