913c7184abd70a41952f5743dc08971ba7138f14
youkidearitai <youkidearitai+github@gmail.com>
2017-06-05 00:19:11 +0900

    42.py 係り元と係り先の文節の表示

---
+++ 42.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+import cabocha_05.cabocha_init
+
+morphs = cabocha_05.cabocha_init.cabocha_file_open()
+
+for index in range(0, len(morphs)):
+    print(
+""" 文節: {0}
+係り先: {1}
+""".format(
+            "\t".join(
+                [morph.surface for morph in morphs[index].morphs if not morph.is_symbol()]
+            ),
+            "\t".join(
+                [morph.surface for morph in morphs[morphs[index].dst].morphs if not morph.is_symbol()]
+            )
+        )
+    )
+
