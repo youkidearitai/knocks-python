9a93b71b14fdaf17bd79e55d16db67d1db03327d
youkidearitai <youkidearitai+github@gmail.com>
2017-06-21 02:27:07 +0900

    45.py 格パターンの正しい方法、ただしコードがおかしい

--- 45.py
+++ 45.py
@@ -8,13 +8,18 @@ morphs = cabocha_05.cabocha_init.cabocha_file_open()
 for index in range(0, len(morphs)):
     #print("{0} -> {1}".format(index, " ".join([str(src) for src in morphs[index].srcs])))
 
-    for src in morphs[index].srcs:
-        for base_word in morphs[src].morphs:
-            if base_word.pos == '動詞':
-                print("{0} -> {1}".format(
-                    base_word.base,
-                    " ".join([dest.base for dest in morphs[index].morphs if dest.pos == '助詞'])
-                ))
+    for base_word in morphs[index].morphs:
+        if base_word.pos == '動詞':
+            src_morphs = [morphs[src].morphs for src in morphs[index].srcs]
+            srcs = []
+
+            for src_morph in src_morphs:
+                srcs.append([src.base for src in src_morph if src.pos == '助詞'])
+
+            print("{0} -> {1}".format(
+                base_word.base,
+                " ".join([" ".join(src) for src in srcs])
+            ))
 
 """
 for index in range(0, len(morphs)):
